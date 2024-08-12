d1f198ccbdbc59b05d15e3446aed1426b96986e3
Briheet Singh Yadav <briheetyadav@gmail.com>
2024-08-12 23:24:19 +0530

    mod

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::fs::OpenOptions;
+mod v4l2;
+
+const DEVICE_NAME: &str = "/dev/video0";
 fn main() {
+    let f = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(DEVICE_NAME)
+        .unwrap();
+
     println!("Hello, world!");
 }
---
+++ src/v4l2.rs
@@ -0,0 +1,5 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/v4l2-bindings.rs"));
