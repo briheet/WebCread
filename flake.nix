12d7b87c23de6c3d2bee9482e1ec5c97b40b9ac7
briheet <briheetyadav@gmail.com>
2026-01-28 23:09:12 +0530

    add: flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A flake for running the project ig ?";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "WebCread";
+            version = "0.1.0";
+            src = self;
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+        }
+      );
+    };
+
+}
